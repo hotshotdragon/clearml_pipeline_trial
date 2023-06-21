212fe18fe33c2e48dda99b2370c798503f7768e7
Sajal Vasal <vasalsajal@live.com>
2023-06-21 16:24:34 +0530

    v3

--- main.py
+++ main.py
@@ -1,28 +1,38 @@
-from clearml.automation.controller import PipelineController
+from clearml.automation.controller import PipelineController, PipelineDecorator
 from clearml import TaskTypes
-
-@PipelineDecorator.component(return_values=['X_train,X_test, y_train, y_test'],
-                             cache=True,TaskTypes.data_processing)
 from train_test_split import *
-
-@PipelineDecorator.component(return_values=['X_train,encoder'],
-                             cache=True,TaskTypes.data_processing)
 from preprocessor import *
+from train import *
+from test import *
+from pipeline import *
+
+@PipelineDecorator.component(return_values=['X_train', 'X_test', 'y_train', 'y_test'],
+                             cache=True, task_type=TaskTypes.data_processing)
+def split_data_task(pickle_url):
+    return step_two(pickle_url)
+
+@PipelineDecorator.component(return_values=['X_train', 'encoder'],
+                             cache=True, task_type=TaskTypes.data_processing)
+def preprocess_data_task(X_train, X_test, y_train, y_test):
+    return step_three(X_train, X_test, y_train, y_test)
 
 @PipelineDecorator.component(return_values=['model'],
-                             cache=True,TaskTypes.training)
-from train import *
+                             cache=True, task_type=TaskTypes.training)
+def train_model_task(X_train, encoder):
+    return step_four(X_train, encoder)
 
 @PipelineDecorator.component(return_values=['accuracy'],
-                             cache=True,TaskTypes.qc)
-from test import *
-
+                             cache=True, task_type=TaskTypes.qc)
+def test_model_task(X_test, model):
+    return step_five(X_test, model)
 
 @PipelineDecorator.pipeline(name='custom_pipeline_test',project='test',version='0.1')
-from pipeline import *
+def pipeline_execute(pickle_url):
+    return executing_pipeline(pickle_url)
 
 if __name__ == '__main__':
     PipelineDecorator.set_default_execution_queue('default')
-
-    executing_pipeline(pickle_url='https://github.com/hotshotdragon/clearml_pipeline_trial/blob/main/data/data.pkl')
+    executing_pipeline(
+        pickle_url='https://github.com/hotshotdragon/clearml_pipeline_trial/blob/main/data/data.pkl')
+    # Set input arguments for the first step   
     print('Process Completed')
